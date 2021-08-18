8be1f1e7e997d31e9a452362524d039b0a767e38
smomin156 <63006524+smomin156@users.noreply.github.com>
2021-08-18 18:24:57 +0530

    Add files via upload

---
+++ EventDemo/Process.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventDemo
+{
+    public class Process
+    {
+        //declaring an event
+        public event EventHandler ProcessCompleted;
+
+        public void StartProcess()
+        {
+            Console.WriteLine("Process started");
+            //
+            OnProcessCompleted(EventArgs.Empty);
+
+        }
+
+        protected virtual void OnProcessCompleted(EventArgs e)
+        {
+            ProcessCompleted?.Invoke(this, e);
+        }
+    }
+
+}
---
+++ EventDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process process = new Process();
+            process.ProcessCompleted += b1ProcessCompleted;
+            process.StartProcess();
+
+            Console.ReadLine();
+        }
+
+        //event handler
+        public static void b1ProcessCompleted(object sender,EventArgs e)
+        {
+            Console.WriteLine("Process Completed");
+        }
+    }
+}
